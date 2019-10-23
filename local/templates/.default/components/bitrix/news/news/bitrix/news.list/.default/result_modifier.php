0d22b438601831229660580b5055f12f5e3d1b6f
Kate <Katrinekatek@gmail.com>
2019-10-23 16:42:36 +0300

    34

--- local/templates/.default/components/bitrix/news/news/bitrix/news.list/.default/result_modifier.php
+++ local/templates/.default/components/bitrix/news/news/bitrix/news.list/.default/result_modifier.php
@@ -5,5 +5,6 @@ if($arParams["SPECIALDATE"]=="Y")
     foreach ($arResult['ITEMS'] as $ITEM)
     {
         $APPLICATION->SetPageProperty("SPECIALDATE", $ITEM['DISPLAY_ACTIVE_FROM']);
+        break;
     }
 }
